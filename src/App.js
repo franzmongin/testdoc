a4860420b4a99f7ea5c37b4ebc46a6d6ad7cea81
franzmongin <franzmongin>
2021-01-22 06:51:31 +0100

    begin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { waitForDomChange } from '@testing-library/react';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [persons, setPersons] = useState([]);
+    
+    const getPersons = async () => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => {
+            return res.json();
+        })
+        .then( data => {
+            setPersons(data);
+        })
+        
+    }
+    const handleDelete = (id) => {
+        const newPersons = persons.filter(person => person.id !== id)
+        setPersons(newPersons)
+    }
+    useEffect(() => {
+        getPersons();
+    }, []);
+    return (
+        <div className="container source">
+            <div className="persons">
+            <ul>
+                
+                { persons && persons.map((item) => {
+                    return(
+                    <li key={item.id}>
+                        <p>{item.id}</p>
+                        <p>{item.name}</p>
+                        <p>{item.username}</p>
+                        <p>{item.email}</p>
+                        <button onClick={() => handleDelete(item.id)}>delete</button>
+                    </li>
+                    )
+                })}
+            </ul>
+            </div>
+            <form action="">
+            <label htmlFor="personInput"> Nom de la personne</label><br/>
+            <input type="text" name="personInput"/><br/>
+            <button type="submit">fetch data mothafucka</button>
+            </form>
+        </div>
+    )
 }
-
-export default App;
