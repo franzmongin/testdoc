802b82f149f8c6a8026b88814c974cbf99d004fb
mafrax <franz.mongin@gmail.com>
2021-03-16 17:45:29 +0100

    wip

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,28 +1,31 @@
 import React, {useState} from 'react'
 
 export default function LoginPage() {
-    const [credentials, setCredentials] = useState({
-        username: '',
-        password: ''
-    })
-    const handleChange = (e) => {
-        const value = e.currentTarget.value;
-        const name = e.currentTarget.name;
-        setCredentials({ ...credentials, [name]: value });
+    const [emailValue, setemailValue] = useState("coucou");
+    const [passwordValue, setpasswordValue] = useState("coucou");
+    
+    const handleEmailChange = (e) => {
+        setemailValue(e.target.value);
+    }
+    const handlePasswordChange = (e) => {
+        setpasswordValue(e.target.value)
     }
     const handleSubmit =  async (e) => {
         try{
+            // console.log(emailValue);
+            // console.log(passwordValue);
+            const bodyValue = JSON.stringify({username: emailValue, password: passwordValue})
+            // console.log(JSON.stringify(bodyValue));
             e.preventDefault();
-            const response = await fetch(`http://localhost:8000/api/login_check`, {
+            const token = await fetch(`http://localhost:8000/api/login_check`, {
               method: "POST",
               headers: { "Content-type": "application/json" },
-              body: JSON.stringify({
-                "username": String({}),
-                "password": String({})
-              }),
+              body: bodyValue,
+            }).then(function(response){
+                return response.json()
             });
-            console.log(await response.json())
-            return await response.json();
+            console.log(token)
+            window.localStorage.setItem("authToken", token)
           }catch (err) {
             e.preventDefault();
             console.log(err);
@@ -32,10 +35,10 @@ export default function LoginPage() {
         <div>
             <form>
                 <label>Email
-                    <input type="text" value={credentials.username} onChange={handleChange}/>
+                    <input type="text" value={emailValue} onChange={handleEmailChange}/>
                 </label>
                 <label>Password
-                    <input type="text" value={credentials.password} onChange={handleChange}/>
+                    <input type="text" value={passwordValue} onChange={handlePasswordChange}/>
                 </label>
                 <input type="submit" value="submit" onClick={handleSubmit}/>
             </form>
